06a7125680192ced0089bf1f2d10c3a3366f092b
Chermen Ailarov <chermenailarov@Chermens-MacBook-Pro.local>
2025-10-26 21:18:47 +0300

    first letter capit

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 let phrase = 'Chevrolet corvet c6r is a fast car!';
 let phraseLength = phrase.length;
 console.log(phraseLength);
+
+
+
+let capital = 'ya tvoy rot programmiroval!';
+let firstLetter = capital.charAt(0).toUpperCase();
+
+let nonCapital = capital.slice(1);
+
+const result = firstLetter + nonCapital;
+
+console.log(result);
