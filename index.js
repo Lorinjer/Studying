4e8a07c8e76199a221064ee248d2d09b30703280
Chermen Ailarov <chermenailarov@Chermens-MacBook-Pro.local>
2025-11-14 17:58:06 +0300

    Palindromes

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
-let phrase = 'Chevrolet corvet c6r is a fast car!';
-let phraseLength = phrase.length;
-console.log(phraseLength);
+let parent = document.querySelector('.parent');
+
+const horses = [
+    {
+        name: "Russian Heavy",
+        age: 2,
+        price: "50 000 $",
+        image: "https://web-zoopark.ru/loshadi/russkaya_tyazhelovoznaya.html"
+    },
 
 
-
-let capital = 'ya tvoy rot programmiroval!';
-let firstLetter = capital.charAt(0).toUpperCase();
-
-let nonCapital = capital.slice(1);
-
-const result = firstLetter + nonCapital;
-
-console.log(result);
+    {
+        name: "Arabian",
+        age: 3,
+        price: "170 000 $",
+        image: "https://kbpravda.ru/sites/kbpravda.ru/files/styles/700/public/field/image/Besscen..jpg?itok=nz9JsENX"
+    },
 
 
+    {
+        name: "Australian Stock Horse",
+        age: 2,
+        price: "79 000 $",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBGuoO4S70e1_qc5QDbvMdKdJ3ZM_7rDEEMw&s"
+    },
 
 
-function equalReverse(str) {
-    let reverseWord = str.split('').reverse().join('');
-    return str === reverseWord;
-}
+    {
+        name: "Chiniese",
+        age: 2,
+        price: "42 000 $",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPUWmqiTw_WV2mddzClQqhN5hir7RAm7CTQw&s"
+    }
+];
 
-console.log(equalReverse('довод'));
-console.log(equalReverse('tenet'));
-console.log(equalReverse('pineapple'));
-console.log(equalReverse('улыбоктебедедмакар'));
-console.log(equalReverse('аргентинаманитнегра'));
-console.log(equalReverse('ковалпопполавок'));
-console.log(equalReverse('программист'));
+horses.forEach((element) => {
+    const horse = document.createElement('div');
+    horse.classList.add('exampler');
+
+    horse.innerHTML = `
+        <img src="${element.image}" alt="{element.name}">
+        <div class="horse-description">
+            <h3>${horse.name}</h3>
+            <p>Возраст: ${horse.age}</p>
+            <p>Цена: ${horse.price}</p>
+        </div>
+    `
+
+    parent.appendchild(horse);
+});
